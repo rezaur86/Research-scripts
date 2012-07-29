1ab24f074a531ce16d9b7686eec10454f41afd8e
Mohammad Rezaur Rahman <rezaur@rezaur-PC.(none)>
2012-07-29 11:42:10 -0700

    keyword_extraction with csv file writing

--- fb_post/WallpostAnalyzer/extract_keyword.py
+++ fb_post/WallpostAnalyzer/extract_keyword.py
@@ -2,6 +2,7 @@ import os, sys
 import re
 from topia.termextract import extract
 import psycopg2
+import csv
 sys.path.append(os.path.abspath('../jsonParser/'))
 from db_connection import openDb, closeDB
 
@@ -39,20 +40,15 @@ def read_story (group_id, by_post = False):
             cut_off = 100
             cursor.execute('''select parent_message_row_id, string_agg(ARRAY_TO_STRING(ARRAY[name,description,caption,text], ', '),E'\n') from message where fb_wall_row_id = %s group by parent_message_row_id''', (group_row_id,))
             for record in cursor:
-                print record[0]
                 story = record[1]
-                print story
                 story_keywords = extract_keyword(story)
-                i = 0
                 for each_keyword in story_keywords:
                     keyword_row_id = register_keywords(" ".join(re.split("[^a-zA-Z]+", each_keyword[0])))
                     keyword_post_row_id += 1
                     new_keyword_post.append((keyword_post_row_id, keyword_row_id, record[0], each_keyword[1]))    
-                    i += 1
-                print i
-                cut_off -= 1
-                if cut_off < 1:
-                    break     
+#                cut_off -= 1
+#                if cut_off < 1:
+#                    break     
         closeDB(conn, cursor)
         if (by_post == False):
             return story
@@ -126,5 +122,16 @@ init_extraction()
 
 for each_group in groups:
     read_story(each_group, True)
+    print each_group,'done'
 
-push_new_data()
+#push_new_data()
+
+keyword_file =  open ("keyword.csv", 'w')
+writer = csv.writer(keyword_file, quoting=csv.QUOTE_MINIMAL)
+writer.writerows(new_keywords)
+keyword_file.close()
+
+keyword_post_file =  open ("keyword_post.csv", 'w')
+writer = csv.writer(keyword_post_file, quoting=csv.QUOTE_MINIMAL)
+writer.writerows(new_keyword_post)
+keyword_post_file.close()
