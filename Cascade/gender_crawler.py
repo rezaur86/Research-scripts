83d0785a1cc67a5b3b0d6c5855eb8a01f7cd0f1a
Mohammad Rezaur Rahman <rezaur@rezaur-PC.(none)>
2012-10-12 12:28:17 -0700

    gender crawler for multiple file

--- Cascade/gender_crawler.py
+++ Cascade/gender_crawler.py
@@ -29,7 +29,7 @@ def extract_gender(data):
         for an_id in onedata.keys():
             if onedata[an_id].has_key("gender"):
                 batch_genders.append((an_id,onedata[an_id]["gender"]))
-    except Exception as e:
+    except Exception, e:
         print "Gender Extracting, Json Error: %s"%e
     return batch_genders
 
@@ -80,9 +80,9 @@ class CrawlerThread (threading.Thread):
         self.threadID = threadID
         self.name = name
     def run(self):
-        print "Starting " + self.name
+#        print "Starting " + self.name
         UserInfoWorker()
-        print "Exiting " + self.name
+#        print "Exiting " + self.name
 
 def UserInfoWorker():
     global output
@@ -129,56 +129,58 @@ def UserInfoWorker():
             batch_genders.extend(extracted_genders)
         with output_lock:
             output.extend(batch_genders)
-                            
-user_file = open(sys.argv[1], "r")
-user_ids = []
-user_id_pop_lock = threading.RLock()
-for line in user_file:
-    line = line.replace("\n","")    
-    user_ids.append(long(line.strip()))    
-user_file.close()
 
-total_users = len(user_ids)
-print "Total user %s"%total_users
-# Needs another lock to protect output
-output = []
-output_lock = threading.RLock()
-gender_absent_ids = []
-gender_absent_lock = threading.RLock()
-
-thread_ids = []
-for i in range(THREAD_COUNT):
-    thread = CrawlerThread(i, "Thread-"+str(i))
-    thread_ids.append(thread)
-    thread.start()    # Start new Threads
-
-while True:
-    if len(user_ids) > 0:
-        print len(user_ids)*100/total_users,'%'
-        time.sleep(20)
-        if len(output) > 10000:
-            with output_lock:
-                o_gender_json = open (sys.argv[2], "a")
-                writer = csv.writer(o_gender_json, quoting=csv.QUOTE_MINIMAL)
-                writer.writerows(output)
-                o_gender_json.close()
-                output = []
-                
-    else:
-        break
-for a_thread in thread_ids:
-    a_thread.join()
-
-if len(output) > 0:
-    with output_lock:
-        o_gender_json = open (sys.argv[2], "a")
-        writer = csv.writer(o_gender_json, quoting=csv.QUOTE_MINIMAL)
-        writer.writerows(output)
-        o_gender_json.close()
-if len(sys.argv) > 3:
-    o_absent_ids = open (sys.argv[3], "w")
-    for an_id in gender_absent_ids:
-        o_absent_ids.write("%s\n" %an_id)
-    o_absent_ids.close()
-
-print "Exiting Main Thread"
+for each_entry in os.listdir(sys.argv[1]):
+    print each_entry
+    user_file = open(sys.argv[1]+'/'+each_entry, "r")
+    user_ids = []
+    user_id_pop_lock = threading.RLock()
+    for line in user_file:
+        line = line.replace("\n","")    
+        user_ids.append(long(line.strip()))    
+    user_file.close()
+    
+    total_users = len(user_ids)
+#    print "Total user %s"%total_users
+    # Needs another lock to protect output
+    output = []
+    output_lock = threading.RLock()
+    gender_absent_ids = []
+    gender_absent_lock = threading.RLock()
+    
+    thread_ids = []
+    for i in range(THREAD_COUNT):
+        thread = CrawlerThread(i, "Thread-"+str(i))
+        thread_ids.append(thread)
+        thread.start()    # Start new Threads
+    
+    while True:
+        if len(user_ids) > 0:
+            print len(user_ids)*100/total_users,'%'
+            time.sleep(20)
+            if len(output) > 10000:
+                with output_lock:
+                    o_gender_json = open (sys.argv[2]+'/'+each_entry, "a")
+                    writer = csv.writer(o_gender_json, quoting=csv.QUOTE_MINIMAL)
+                    writer.writerows(output)
+                    o_gender_json.close()
+                    output = []
+                    
+        else:
+            break
+    for a_thread in thread_ids:
+        a_thread.join()
+    
+    if len(output) > 0:
+        with output_lock:
+            o_gender_json = open (sys.argv[2]+'/'+each_entry, "a")
+            writer = csv.writer(o_gender_json, quoting=csv.QUOTE_MINIMAL)
+            writer.writerows(output)
+            o_gender_json.close()
+    if len(sys.argv) > 3:
+        o_absent_ids = open (sys.argv[3]+'/'+each_entry, "w")
+        for an_id in gender_absent_ids:
+            o_absent_ids.write("%s\n" %an_id)
+        o_absent_ids.close()
+    
+#    print "Exiting Main Thread"
