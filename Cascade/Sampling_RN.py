6e57f3e6b93ae09d577f6c8c8a453d52a5713eda
Mohammad Rezaur Rahman <mohammra@cisco.com>
2014-02-07 16:35:47 -0800

    using -1 as users not sampled

--- Cascade/Sampling_RN.py
+++ Cascade/Sampling_RN.py
@@ -6,12 +6,15 @@ import math
 import random
 import array
 
+total_users = 189989307
+total_activities = 2027488267
+
+SAMPLE_SIZE = int(total_users*.1)
+sampled_graph_nodes = set() #Final size of this set has to be equal to the SAMPLE_SIZE
+
 def WeightedSelectionWithoutReplacement(weights, m):
     elt = [(math.log(random.random()) / weights[i], i) for i in range(len(weights))]
     return [x[1] for x in heapq.nlargest(m, elt)]
-
-total_users = 189989307
-total_activities = 2027488267
 
 each_app = sys.argv[1]
 # sampling method RN = 0
@@ -37,10 +40,12 @@ if sampling_method == 0:
             sampled_users.add(int(user_id))
     except:
         # Random Node Sampling
-        sampled_users = set(sample(xrange(total_users), int(total_users*.1)))
+        print "Sampling..."
+        sampled_users = set(sample(xrange(total_users), SAMPLE_SIZE))
         w = csv.writer(open("/home/rezaur/data/sampled_users/RN.txt", "w"))
         for user_id in sampled_users:
             w.writerow([user_id]) 
+        print "Sampling Done"
 
 if sampling_method == 2:
     each_app_anonym = each_app + '_sampled_RDN'
@@ -51,36 +56,58 @@ if sampling_method == 2:
         weights = array.array('l',(0,)*total_users)
         for user_id,odeg,indeg,deg in csv.reader(open('/home/rezaur/data/iheart_ext_preprocessed_basic.txt')):
             weights[int(user_id)] = int(deg)
-        sampled_users = set(WeightedSelectionWithoutReplacement(weights, int(total_users*.1)))
+        print "Sampling..."
+        sampled_users = set(WeightedSelectionWithoutReplacement(weights, SAMPLE_SIZE))
+        w = csv.writer(open("/home/rezaur/data/sampled_users/RDN.txt", "w"))
+        for user_id in sampled_users:
+            w.writerow([user_id]) 
+        print "Sampling Done"
 
 if sampling_method == 3:
     each_app_anonym = each_app + '_sampled_RE'
     try:
         for activity_line in csv.reader(open(sys.argv[3])): #activity lines are edges
-            sampled_act.add(activity_line)
+            sampled_act.add(int(activity_line))
     except:
         # Random Edge Sampling
-        sampled_act = set(sample(xrange(total_activities), int(total_users*.1)))
-        w = csv.writer(open("/home/rezaur/data/sampled_users/RE.txt", "w"))
-        for user_id in sampled_users:
-            w.writerow([user_id]) 
+        print "Sampling..."
+        sampled_act = set(sample(xrange(total_activities), int(total_activities*.1)))
+#         w = csv.writer(open("/home/rezaur/data/sampled_users/RE.txt", "w"))
+#         for act in sampled_act:
+#             w.writerow([act]) 
+        print "Sampling Done"
 
 if sampling_method == 4:
     each_app_anonym = each_app + '_sampled_RNE'
     # Random Node-Edge Sampling
-    sampled_users = set(sample(xrange(total_users), int(total_users*.1)))
+    print "Sampling..."
+    sampled_users = set(sample(xrange(total_users), int(total_users*.01)))
     for user_id,odeg,indeg,deg in csv.reader(open(sys.argv[3])):
-        if user_id in sampled_users:
-            sampled_users_act[user_id] = sample(xrange(deg), 1)
+        if int(user_id) in sampled_users:
+            sampled_users_act[int(user_id)] = sample(xrange(int(deg)), 1)
     sampled_users = set([])
     w = csv.writer(open("/home/rezaur/data/sampled_users/RNE.txt", "w"))
     for user_id in sampled_users_act:
-        w.writerow([user_id,sampled_users_act[user_id]]) 
+        w.writerow([user_id,sampled_users_act[user_id]])
+    print "Sampling Done"
 
 if sampling_method == 6:
     each_app_anonym = each_app + '_sampled_RNN'
-    for user_id in csv.reader(open(sys.argv[3])):
-        sampled_senders.add(user_id)
+    try:
+        for user_id in csv.reader(open(sys.argv[3])):
+            sampled_senders.add(int(user_id))
+    except:
+        non_zero_odeg_users = []
+        for user_id,odeg,indeg,deg in csv.reader(open('/home/rezaur/data/iheart_ext_preprocessed_basic.txt')):
+            if int(odeg) > 0:
+                non_zero_odeg_users.append(int(user_id))
+        total_parents = len(non_zero_odeg_users) 
+        print "Sampling..."
+        sampled_senders = set(sample(non_zero_odeg_users, int(total_parents*.01)))
+        w = csv.writer(open("/home/rezaur/data/sampled_users/RDN.txt", "w"))
+        for user_id in sampled_senders:
+            w.writerow([user_id]) 
+        print "Sampling Done"
 
 users = {}
 user_count = 0
@@ -103,8 +130,14 @@ for each_file in file_list:
         sender_org_id = long(splits[0].strip())
         recv_org_id = long(splits[1].strip())
         if len(sampled_users) != 0:
-            if sender_org_id in sampled_users and recv_org_id in sampled_users:
+            if sender_org_id in sampled_users:
                 take_this_act = True
+            else:
+                users[sender_org_id] = -1
+            if recv_org_id in sampled_users:
+                take_this_act = True
+            else:
+                users[recv_org_id] = -1
         if len(sampled_users_act) != 0:
             if sender_org_id in sampled_users_act:
                 if sampled_users_act[sender_org_id] != 0:
@@ -116,13 +149,22 @@ for each_file in file_list:
                     sampled_users_act[recv_org_id] -= 1
                 else:
                     take_this_act = True
-        if len(sampled_act) != 0:
+        if len(sampled_act) > 0:
             if (activity_line-1) in sampled_act:
+                sampled_act.remove(activity_line-1)
                 take_this_act = True
         if len(sampled_senders) != 0:
             if sender_org_id in sampled_senders:
                 take_this_act = True
 
+        # If desired sample size is already met then we don't want to
+        # visit any new nodes, but we can explore the edges
+        if len(sampled_graph_nodes) == SAMPLE_SIZE:
+            if sender_org_id not in sampled_graph_nodes or recv_org_id not in sampled_graph_nodes:
+                take_this_act = False
+        if len(sampled_graph_nodes) == (SAMPLE_SIZE - 1):
+            if sender_org_id not in sampled_graph_nodes and recv_org_id not in sampled_graph_nodes:
+                take_this_act = False
         if take_this_act == False:
             continue        
         if users.has_key(sender_org_id) == False:
@@ -136,6 +178,8 @@ for each_file in file_list:
         timestamp = splits[2].strip()
         symid = splits[3].strip()
         anonymized_data.append((sender, recv, timestamp, symid))
+        sampled_graph_nodes.add(sender_org_id)
+        sampled_graph_nodes.add(recv_org_id)
     anonymized_file = open(each_app_anonym+'/'+each_file, "wb")
     writer = csv.writer(anonymized_file, quoting=csv.QUOTE_MINIMAL, delimiter=' ')
     writer.writerows(anonymized_data)
--- Cascade/node_ext_info.py
+++ Cascade/node_ext_info.py
@@ -31,14 +31,16 @@ if __name__ == '__main__':
             sender = long(splits[0].strip())
             recv = long(splits[1].strip())
             timestamp = long(splits[2].strip())
-            if len(user_last_seen_time) > sender:
-                user_last_seen_time[sender] = max(user_last_seen_time[sender], activity_line)
-            else:
-                user_last_seen_time.append(activity_line)          
-            if len(user_last_seen_time) > recv:
-                user_last_seen_time[recv] = max(user_last_seen_time[recv], activity_line)
-            else:
-                user_last_seen_time.append(activity_line)
+            if sender != -1:
+                if len(user_last_seen_time) > sender:
+                    user_last_seen_time[sender] = max(user_last_seen_time[sender], activity_line)
+                else:
+                    user_last_seen_time.append(activity_line)          
+            if recv != -1:
+                if len(user_last_seen_time) > recv:
+                    user_last_seen_time[recv] = max(user_last_seen_time[recv], activity_line)
+                else:
+                    user_last_seen_time.append(activity_line)
         f.close()
         print each_file
     total_line = activity_line
@@ -57,51 +59,70 @@ if __name__ == '__main__':
             sender = long(splits[0].strip())
             recv = long(splits[1].strip())
             timestamp = long(splits[2].strip())
-            
-            if(sender > vertices_count - 1):
-                vertices_count += 1
-                potential_parents.append(NO_PARENT)
-                born_time.append(timestamp)
-                activation_time.append(timestamp)
-                user_last_act_time.append(timestamp)
-                out_degree.append(1)
-                in_degree.append(0)
-            else:
-                out_degree[sender] += 1 # Raising out degree even if multiple sending to same node ??
-                if(out_degree[sender]) == 1:
-                    activation_time[sender] = timestamp # Activation time is the earliest sending time
-                    user_last_act_time[sender] = timestamp
+            if sender != -1 and recv != -1:
+                if(sender > vertices_count - 1):
+                    vertices_count += 1
+                    potential_parents.append(NO_PARENT)
+                    born_time.append(timestamp)
+                    activation_time.append(timestamp)
+                    user_last_act_time.append(timestamp)
+                    out_degree.append(1)
+                    in_degree.append(0)
                 else:
-                    user_last_act_time[sender] = max(user_last_act_time[sender], timestamp)
+                    out_degree[sender] += 1 # Raising out degree even if multiple sending to same node ??
+                    if(out_degree[sender]) == 1:
+                        activation_time[sender] = timestamp # Activation time is the earliest sending time
+                        user_last_act_time[sender] = timestamp
+                    else:
+                        user_last_act_time[sender] = max(user_last_act_time[sender], timestamp)
+                is_leaf[sender] = False
             
-            is_leaf[sender] = False
+                if(recv > vertices_count - 1):
+                    vertices_count += 1
+                    potential_parents.append([])
+                    potential_parents[recv].append((sender,timestamp))
+                    born_time.append(timestamp)
+                    activation_time.append(NEVER) # Not yet Activated
+                    user_last_act_time.append(NEVER)
+                    out_degree.append(0)
+                    in_degree.append(1)
+                else:
+                    in_degree[recv] += 1 # Raising in degree even if multiple receiving at same node ??
+                    if out_degree[recv] < 1:
+                        sender_is_already_parent = False
+                        for (p,t) in potential_parents[recv]:
+                            if sender == p:
+                                sender_is_already_parent = True
+                                break
+                        if sender_is_already_parent == False:
+                            potential_parents[recv].append((sender,timestamp))
             
-            if(recv > vertices_count - 1):
-                vertices_count += 1
-                potential_parents.append([])
-                potential_parents[recv].append((sender,timestamp))
-                born_time.append(timestamp)
-                activation_time.append(NEVER) # Not yet Activated
-                user_last_act_time.append(NEVER)
-                out_degree.append(0)
-                in_degree.append(1)
-            else:
-                in_degree[recv] += 1 # Raising in degree even if multiple receiving at same node ??
-                if out_degree[recv] < 1:
-                    sender_is_already_parent = False
-                    for (p,t) in potential_parents[recv]:
-                        if sender == p:
-                            sender_is_already_parent = True
-                            break
-                    if sender_is_already_parent == False:
-                        potential_parents[recv].append((sender,timestamp))
+                if user_last_seen_time[sender]==activity_line:
+                    users_done.append(sender)
+                    user_last_seen_time[sender] = timestamp #Replacing the last seen line number with last seen time
+                if sender!=recv and user_last_seen_time[recv]==activity_line:
+                    users_done.append(recv)
+                    user_last_seen_time[recv] = timestamp #Replacing the last seen line number with last seen time
             
-            if user_last_seen_time[sender]==activity_line:
-                users_done.append(sender)
-                user_last_seen_time[sender] = timestamp #Replacing the last seen line number with last seen time
-            if sender!=recv and user_last_seen_time[recv]==activity_line:
-                users_done.append(recv)
-                user_last_seen_time[recv] = timestamp #Replacing the last seen line number with last seen time
+            if recv == -1:
+                if(sender > vertices_count - 1):
+                    vertices_count += 1
+                    potential_parents.append(NO_PARENT)
+                    born_time.append(timestamp)
+                    activation_time.append(NEVER)
+                    user_last_act_time.append(NEVER)
+                    out_degree.append(0)
+                    in_degree.append(0)
+            if sender == -1:
+                if(recv > vertices_count - 1):
+                    vertices_count += 1
+                    potential_parents.append(NO_PARENT)
+                    born_time.append(timestamp)
+                    activation_time.append(NEVER) # Not yet Activated
+                    user_last_act_time.append(NEVER)
+                    out_degree.append(0)
+                    in_degree.append(0)
+
             if len(users_done) >= 10000 or activity_line==total_line:
                 for i in users_done:
                     o_f.write('%s %s %s %s %s %s %s %s'%(i, born_time[i], activation_time[i] if is_leaf[i]==False else (user_last_seen_time[i]+1) # anything bigger than last seen time
