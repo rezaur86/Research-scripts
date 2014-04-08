a08ff4560f08d329943c4d021f2320297373c39e
Mohammad Rezaur Rahman <mohammra@cisco.com>
2014-04-08 11:33:15 -0700

    raw indeg and outdeg together

--- Cascade/raw_stat_analysis.r
+++ Cascade/raw_stat_analysis.r
@@ -26,16 +26,33 @@ lifespan_analysis <- function(lifespan_file_name){
 	return(parent_lifespan)
 }
 
-raw_outdeg_analysis <- function (raw_outdeg_file_name){
-	raw_outdeg <<- as.data.frame(read.csv(raw_outdeg_file_name, header=FALSE))
-	colnames(raw_outdeg) <<- c('outdeg', 'count')
-	raw_outdeg <- raw_outdeg[order(raw_outdeg$outdeg),]
-	raw_outdeg$cum_count <- cumsum(raw_outdeg$count)
-	raw_outdeg$cdf <- raw_outdeg$cum_count / max(raw_outdeg$cum_count)
-	plot <- ggplot(raw_outdeg, aes(x = outdeg, y = cdf)) + geom_line() +
-			xlab('Number of sent ARs') + ylab('Empirical CDF') + #+ scale_y_log10()
-			scale_x_log10(limits = c(1, 1000))
-	save_ggplot(plot, 'raw_stat_v2/raw_outdeg.pdf')
+raw_outdeg_analysis <- function (raw_outdeg_file_name = 'raw_stat_v2/raw_outdeg_stat.csv',
+		raw_indeg_file_name='raw_stat_v2/raw_indeg_stat.csv'){
+	raw_outdeg <- as.data.frame(read.csv(raw_outdeg_file_name, header=FALSE))
+	raw_outdeg[,3] <- 0
+	raw_indeg <- as.data.frame(read.csv(raw_indeg_file_name, header=FALSE))
+	raw_indeg[,3] <- 1
+	raw_degree <- rbind(raw_outdeg, raw_indeg)
+	colnames(raw_degree) <- c('degree', 'count', 'degree_type')
+	raw_degree.df <- ddply(raw_degree, c('degree_type'), function(one_partition){
+				one_partition = one_partition[order(one_partition$degree),]
+				one_partition$cum_count = cumsum(one_partition$count)
+				one_partition$cdf_val = one_partition$cum_count / max(one_partition$cum_count)
+				one_partition$pdf_val = one_partition$count / max(one_partition$cum_count)
+				one_partition
+			})
+	raw_degree.df$degree_type <- factor(raw_degree.df$degree_type)
+	print(summary(raw_degree.df))
+#	raw_outdeg <- raw_outdeg[order(raw_outdeg$outdeg),]
+#	raw_outdeg$cum_count <- cumsum(raw_outdeg$count)
+#	raw_outdeg$cdf <- raw_outdeg$cum_count / max(raw_outdeg$cum_count)
+#	raw_outdeg$pdf <- raw_outdeg$count / max(raw_outdeg$cum_count)
+	plot <- ggplot(raw_degree.df[raw_degree.df$degree > 0, ], aes(x = degree, y = pdf_val)) +
+			geom_point(aes(group = degree_type, colour = degree_type, shape = degree_type), size=1) +
+			xlab('Number of sent/received ARs') + ylab('Empirical PDF') + scale_y_log10() +
+			scale_x_log10(limits = c(1, 10^4))
+	plot <- change_plot_attributes_fancy(plot, "", 0:1, c('Out degree', 'In degree'), "Number of sent/received ARs", "Empirical PDF")
+	save_ggplot(plot, 'raw_stat_v2/degree.pdf')
 }
 
 raw_indeg_analysis <- function (raw_indeg_file_name){
@@ -44,9 +61,10 @@ raw_indeg_analysis <- function (raw_indeg_file_name){
 	raw_indeg <- raw_indeg[order(raw_indeg$indeg),]
 	raw_indeg$cum_count <- cumsum(raw_indeg$count)
 	raw_indeg$cdf <- raw_indeg$cum_count / max(raw_indeg$cum_count)
-	plot <- ggplot(raw_indeg, aes(x = indeg, y = cdf)) + geom_line() +
-			xlab('Number of received ARs') + ylab('Empirical CDF') + #+ scale_y_log10()
-			scale_x_log10(limits = c(1, 1000))
+	raw_indeg$pdf <- raw_indeg$count / max(raw_indeg$cum_count)
+	plot <- ggplot(raw_indeg[raw_indeg$indeg > 0, ], aes(x = indeg, y = pdf)) + geom_point(size=.8) +
+			xlab('Number of received ARs') + ylab('Empirical PDF') + scale_y_log10() +
+			scale_x_log10(limits = c(2, 10^4))
 	save_ggplot(plot, 'raw_stat_v2/raw_indeg.pdf')
 }
 
@@ -78,40 +96,70 @@ influence_threshold_analysis <- function (parent_count_file_name, indeg_before_a
 	save_ggplot(plot, 'raw_stat_v2/adop_prob.pdf')
 }
 
-active_proportion_analysis <- function (active_proportion_file){
-	influence <- as.data.frame(read.csv(active_proportion_file, header=FALSE))
-	influence[,3] <- 0
-	colnames(influence) <- c('proportion', 'count','dummy_type')
-	influence.df <- ddply(influence, c('dummy_type'), function(one_partition){
-				one_partition = one_partition[order(one_partition$proportion),]
-				one_partition$cum_count = cumsum(one_partition$count)
-				one_partition$cdf_val = one_partition$cum_count / max(one_partition$cum_count)
-				one_partition$pdf_val = one_partition$count / max(one_partition$cum_count)
-				one_partition
-			})
-	influence.df$dummy_type <- factor(influence.df$dummy_type)
-	plot <- ggplot(influence.df, aes(x = (proportion), y = (cdf_val))) + 
-			geom_point(aes(group = dummy_type, colour = dummy_type, shape = dummy_type), size=1)
-#			scale_x_log10(limits = c(1, 100)) #+ scale_y_log10() #+ theme(legend.position=c(.8, .7)) + xlim(0,log10(plot_x_lim*100))
-	plot <- change_plot_attributes_fancy(plot, "", 0:0, c('Distinct Parent'), "Proportion", "CDF")
-	save_ggplot(plot, 'raw_stat_v2/active_proportion.pdf')
+success_ratio_analysis <- function (success_ratio_file='raw_stat_v2/act_proportion_count.csv'){
+	influence <- as.data.frame(read.csv(success_ratio_file, header=FALSE))
+	colnames(influence) <- c('proportion', 'count')
+	influence <- influence[order(influence$proportion),]
+	influence$cum_count <- cumsum(influence$count)
+	influence$cdf_val <- influence$cum_count / max(influence$cum_count)
+	plot <- ggplot(influence, aes(x = (proportion), y = (cdf_val))) + 
+			geom_line() + xlab('Success Ratio (%)') + ylab('Empirical CDF')
+	save_ggplot(plot, 'raw_stat_v2/success_ratio.pdf')
+}
+
+sender_success_ratio_analysis <- function (success_ratio_file='raw_stat_v2/parent_proportion.csv'){
+	sender_success <- as.data.frame(read.csv(success_ratio_file, header=FALSE))
+	colnames(sender_success) <- c('active_children', 'children')
+	sender_success$ratio <- round(sender_success$active_children/sender_success$children, 3)
+	sender_success.avg_ratio <- ddply(sender_success, c('children'), summarise, avg_success_ratio = mean (ratio))
+	plot <- ggplot(sender_success.avg_ratio, aes(x = (avg_success_ratio*100), y = (children))) + 
+			geom_point() + geom_smooth(method=lm,   # Add linear regression lines
+					se=FALSE) + xlab('Average Success Ratio (%)') + ylab('Number of sent ARs')
+	save_ggplot(plot, 'raw_stat_v2/success_avg_vs_ARs.pdf')
+#	sender_success.avg_ar <- ddply(sender_success, c('ratio'), summarise, avg_children = mean (children))
+#	plot <- ggplot(sender_success.avg_ar, aes(x = (ratio*100), y = (avg_children))) + 
+#			geom_line() + xlab('Success Ratio (%)') + ylab('Average number of sent ARs')
+#	save_ggplot(plot, 'raw_stat_v2/success_vs_avg_ARs.pdf')
+#	return(list(avg_ratio = sender_success.avg_ratio, avg_ar = sender_success.avg_ar))
+	return (sender_success.avg_ratio)
 }
 
 invitation_burstiness <- function(file='raw_stat_v2/invitation_burstiness_stat.csv'){
 	invitations <- as.data.frame(read.csv(file, header=FALSE))
-	colnames(invitations) <- c('burstiness', 'count')
+	colnames(invitations) <- c('burstiness', 'count', 'avg_elapsed_time', 'median_elapsed_time')
 #	invitations$burstiness <- as.numeric(levels(invitations$burstiness))[invitations$burstiness]
 	invitations <- invitations[order(invitations$burstiness), ]
 	invitations$cum_sum <- cumsum(invitations$count)
 	invitations$cdf <- invitations$cum_sum/max(invitations$cum_su)
-	plot <- ggplot(invitations[((invitations$burstiness > -1) & (invitations$burstiness < 1)), ],
-			aes(x = burstiness, y = count)) + geom_line() + xlab('Burstiness') + ylab('Count')
+	plot <- ggplot(invitations,
+			aes(x = burstiness, y = count)) + geom_point(size=.8)+ geom_smooth(#method=lm,   # Add linear regression lines
+			se=FALSE)+ xlab('Burstiness') + ylab('Count')
 #			scale_x_log10() + scale_y_log10()
-	save_ggplot(plot, 'raw_stat_v2/burstiness_count.pdf')
-	plot <- ggplot(invitations[((invitations$burstiness > -1) & (invitations$burstiness < 1)), ],
+	save_ggplot(plot, 'raw_stat_v2/invitation_burstiness_count.pdf')
+	plot <- ggplot(invitations,
 			aes(x = burstiness, y = cdf)) + geom_line() + xlab('Burstiness') + ylab('Empirical CDF')
 #			scale_x_log10() + scale_y_log10()
-	save_ggplot(plot, 'raw_stat_v2/burstiness_cdf.pdf')
+	save_ggplot(plot, 'raw_stat_v2/invitation_burstiness_cdf.pdf')
+	plot <- ggplot(invitations,
+					aes(x = burstiness, y = avg_elapsed_time)) + geom_point(size=.8)+ geom_smooth(#method=lm,
+					se=FALSE)+ xlab('Burstiness') + ylab('Avg. elapsed time')
+	save_ggplot(plot, 'raw_stat_v2/invitation_burstiness_avg_elapse_time.pdf')
+	plot <- ggplot(invitations,
+					aes(x = burstiness, y = median_elapsed_time)) + geom_point(size=.8)+ geom_smooth(#method=lm,
+					se=FALSE)+ xlab('Burstiness') + ylab('Median elapsed time')
+	save_ggplot(plot, 'raw_stat_v2/invitation_burstiness_median_elapsed_time.pdf')
+	return(invitations)
+}
+
+invitation_elapsed_time_analysis <- function (invitation_elapsed_time_file='raw_stat_v2/invitation_elapsed_time_stat.csv'){
+	elapsed_time <- as.data.frame(read.csv(invitation_elapsed_time_file, header=FALSE))
+	colnames(elapsed_time) <- c('time', 'count')
+	elapsed_time <- elapsed_time[order(elapsed_time$time),]
+	elapsed_time$cum_count <- cumsum(elapsed_time$count)
+	elapsed_time$cdf_val <- elapsed_time$cum_count / max(elapsed_time$cum_count)
+	plot <- ggplot(elapsed_time, aes(x = (time), y = (cdf_val))) + 
+			geom_line() + xlab('Elapsed time (Hour)') + ylab('Empirical CDF')
+	save_ggplot(plot, 'raw_stat_v2/invitation_elased_time.pdf')
 }
 
 #parent_lifespan<-lifespan_analysis('raw_stat_1/lifespan_stat.csv')
